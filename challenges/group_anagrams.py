4d1e57d03e29646db365ff8a640f806b73d9e9d8
CarolinaaCabral <carolinacabral@outlook.com.br>
2026-06-17 12:50:18 -0300

    Resolução:Group Anagrams

--- challenges/group_anagrams.py
+++ challenges/group_anagrams.py
@@ -1,2 +1,12 @@
 def group_anagrams(words: list[str]) -> list[list[str]]:
-    raise NotImplementedError
+    anagramas = {}
+
+    for palavra in words:
+        palavra_ordenada = "".join(sorted(palavra))
+
+        if palavra_ordenada not in anagramas:
+            anagramas[palavra_ordenada] = []
+
+        anagramas[palavra_ordenada].append(palavra)
+
+    return list(anagramas.values())
