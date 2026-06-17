c58bd2cd5302c38302ca698faec1b35f5e690f42
CarolinaaCabral <carolinacabral@outlook.com.br>
2026-06-17 12:53:38 -0300

    Resolução: Has Cycle

--- challenges/has_cycle.py
+++ challenges/has_cycle.py
@@ -1,2 +1,21 @@
 def has_cycle(graph: dict[str, list[str]]) -> bool:
-    raise NotImplementedError
+    visitados = set()
+    pilha_recursao = set()
+
+    def dfs(no_atual):
+        pilha_recursao.add(no_atual)
+        for vizinho in graph.get(no_atual, []):
+            if vizinho in pilha_recursao:
+                return True
+            if vizinho not in visitados:
+                if dfs(vizinho):
+                    return True
+        pilha_recursao.remove(no_atual)
+        visitados.add(no_atual)
+        return False
+
+    for vertice in graph:
+        if vertice not in visitados:
+            if dfs(vertice):
+                return True
+    return False
